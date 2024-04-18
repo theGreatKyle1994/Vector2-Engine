4a81393e3aea093f52e3d986298a46372d0ecdc2
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2024-04-18 19:08:34 -0400

    gitignore update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import Scene from "./engine/Scene";
 import GameInstance from "./engine/GameInstance";
-import { default as Vector2 } from "./physics/Vector2";
 
 class myScene extends Scene {
   constructor() {
     super();
   }
   protected init(): void {}
-  protected create(): void {
-    const myVec: Vector2 = new Vector2({ x: 2, y: 3 });
-    const myVec2: Vector2 = new Vector2({ x: 2, y: 2 });
-    console.log(Vector2.componentProduct(myVec, myVec2));
-  }
+  protected create(): void {}
   protected update(ctx: CanvasRenderingContext2D, deltaTime: number): void {}
 }
 
